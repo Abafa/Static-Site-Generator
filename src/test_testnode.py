b2b4aae76df286859e76cacc9737715729234a84
Abafa <flo.beg@free.fr>
2026-06-25 18:50:48 +0200

    did some testy stuff, don't get it's use tho

---
+++ src/test_testnode.py
@@ -0,0 +1,35 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_different_types(self):
+        node = TextNode("hello", TextType.BOLD)
+        node2 = TextNode("hello", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_same_type(self):
+        node = TextNode("hello", TextType.BOLD)
+        node2 = TextNode("hello", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_url(self):
+        node = TextNode("http://www.boot.dev/", TextType.LINKS)
+        node2 = TextNode("http://www.boot.dev/", TextType.LINKS)
+        self.assertEqual(node, node2)
+
+    def test_not_url(self):
+        node = TextNode("http://www.boot.dev/", TextType.LINKS)
+        node2 = TextNode("hello", TextType.ITALIC)
+        self.assertNotEqual(node, node2)  
+
+    
+
+if __name__ == "__main__":
+    unittest.main()
+
+
