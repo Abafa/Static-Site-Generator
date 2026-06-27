7afbf549c71d3e2ec5aae1a50fca3bd755b2d07b
Abafa <flo.beg@free.fr>
2026-06-27 23:15:02 +0200

    LeafNode added

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,3 +20,18 @@ class HTMLNode:
     def __repr__(self):
         return f"HTMLNode(value={self.value}, tag={self.tag}, children={self.children}, props={self.props})"
     
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    
+
+    def to_html(self):
+        if not self.value : 
+            raise ValueError("This node should have a value !")
+        if self.tag == None : 
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"HTMLNode(value={self.value}, tag={self.tag}, props={self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
@@ -30,6 +30,17 @@ class TestHTMLNode(unittest.TestCase):
         result = node.props_to_html()
         self.assertEqual(result, expected)
 
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_with_url(self):
+        node = LeafNode("a", "Click Me !", {"href" : "https://www.boots.dev/"})
+        self.assertEqual(node.to_html(), '<a href="https://www.boots.dev/">Click Me !</a>')
+
+    def test_leaf_to_html_but_bold(self):
+        node = LeafNode("b", "Hello, world!")
+        self.assertEqual(node.to_html(), "<b>Hello, world!</b>")
 
 
 if __name__ == "__main__":
