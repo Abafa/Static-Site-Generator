fb9c4187eeb9a77b052265cbf64b59e1c5c408ad
Abafa <flo.beg@free.fr>
2026-06-30 20:50:44 +0200

    working text node to html node function added

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -18,14 +19,33 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
 
     def test_url(self):
-        node = TextNode("http://www.boot.dev/", TextType.LINKS)
-        node2 = TextNode("http://www.boot.dev/", TextType.LINKS)
+        node = TextNode("http://www.boot.dev/", TextType.LINK)
+        node2 = TextNode("http://www.boot.dev/", TextType.LINK)
         self.assertEqual(node, node2)
 
     def test_not_url(self):
-        node = TextNode("http://www.boot.dev/", TextType.LINKS)
+        node = TextNode("http://www.boot.dev/", TextType.LINK)
         node2 = TextNode("hello", TextType.ITALIC)
         self.assertNotEqual(node, node2)  
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_link(self):
+        node = TextNode("http://www.boot.dev/", TextType.LINK, "http://www.boot.dev/")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "http://www.boot.dev/")
+        self.assertEqual(html_node.props, {"href":"http://www.boot.dev/"})
+
+    def test_italic(self):
+        node = TextNode("YOLO", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "YOLO")
 
     
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,16 +1,17 @@
 from enum import Enum
+from htmlnode import LeafNode, HTMLNode, ParentNode
 
 class TextType(Enum):
-    PLAIN = 1
+    TEXT = 1
     BOLD = 2
     ITALIC = 3
     CODE = 4
-    LINKS = 5
-    IMAGES = 6
+    LINK = 5
+    IMAGE = 6
 
 
 class TextNode :
-    def __init__(self, text: str, text_type: TextType = TextType.PLAIN, url: str = None):
+    def __init__(self, text: str, text_type: TextType = TextType.TEXT, url: str = None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -22,3 +23,20 @@ class TextNode :
     
     def __repr__(self):
         return f"TextNode(text={self.text}, text_type={self.text_type}, url={self.url})"
+
+
+def text_node_to_html_node(text_node : TextNode) -> LeafNode : 
+    if text_node.text_type not in TextType :
+        raise Exception ("Text Type error !")
+    if text_node.text_type is TextType.TEXT :
+        return LeafNode(None, text_node.text, None)
+    if text_node.text_type is TextType.BOLD :
+        return LeafNode("b", text_node.text, None)
+    if text_node.text_type is TextType.ITALIC :
+        return LeafNode("i", text_node.text, None)
+    if text_node.text_type is TextType.CODE :
+        return LeafNode("code", text_node.text, None)
+    if text_node.text_type is TextType.LINK :
+        return LeafNode("a", text_node.text, {"href":text_node.text})
+    if text_node.text_type is TextType.IMAGE :
+        return LeafNode("img", None, {"src":text_node.url, "alt":text_node.text})
