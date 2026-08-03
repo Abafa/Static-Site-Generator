ebba72e0ae103f246decb0c8c1c92d4372ca1303
Abafa <flo.beg@free.fr>
2026-08-03 23:07:05 +0200

    meh

---
+++ src/bloctype.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ORDERED_LIST = "ordered_list"
+    UNORDERED_LIST = "unordered_list"
+
+
+def block_to_block_type(block: str) -> BlockType:
+    prefix = ("#", "##", "###", "####", "#####", "######")
+    if block.startswith(prefix):
+        return BlockType.HEADING
+    if block.startswith(">"):
+        return BlockType.QUOTE
+    if block.startswith("```"):
+        return BlockType.CODE
+    splited_block = block.split("\n")
+    if all(line.startswith("- ") for line in splited_block):
+        return BlockType.UNORDERED_LIST
+    if all(line.split(".")[0].isdigit() for line in splited_block):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
