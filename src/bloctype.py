e96d9091ea6a5af52fcb8f26289830bea0c013f8
Abafa <flo.beg@free.fr>
2026-08-09 21:58:46 +0200

    humhum

--- src/bloctype.py
+++ src/bloctype.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from inline_markdown import markdown_to_blocks
-from HTMLNode import HTMLNode
+from htmlnode import HTMLNode
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -32,32 +32,46 @@ def markdown_to_html_node(markdown) :
     splited_markdown = markdown_to_blocks(markdown)
     block_types = [block_to_block_type(block) for block in splited_markdown]
     for block, block_type in zip(splited_markdown, block_types):
+        if block_type == BlockType.PARAGRAPH:
+            yield to_paragraph(block)
+        elif block_type == BlockType.HEADING:
+            yield to_heading(block)
+        elif block_type == BlockType.CODE:
+            yield to_code(block)
+        elif block_type == BlockType.QUOTE:
+            yield to_blockquote(block)
+        elif block_type == BlockType.ORDERED_LIST:
+            yield to_ordered_list(block)
+        elif block_type == BlockType.UNORDERED_LIST:
+            yield to_unordered_list(block)
+
+    return HTMLNode("div", None, list(markdown_to_html_node(markdown)), False)
 
 
 
 
 def to_blockquote(block: str) -> str:
     new_block = block[1:].strip()
-    return f"<blockquote>{new_block}</blockquote>"
+    return HTMLNode("blockquote", None, new_block, False)
 
 def to_code(block: str) -> str:
     new_block = block[3:-3].strip()
-    return f"<pre><code>{new_block}</code></pre>"
+    return HTMLNode("pre", None, HTMLNode("code", None, new_block, False), False)
 
 def to_heading(block: str) -> str:
     level = block.count("#")
     new_block = block[level:].strip()
-    return f"<h{level}>{new_block}</h{level}>"
+    return HTMLNode(f"h{level}", None, new_block, False)
 
 def to_paragraph(block: str) -> str:
-    return f"<p>{block.strip()}</p>"
+    return HTMLNode("p", None, block.strip(), False)
 
 def to_ordered_list(block: str) -> str:
     splited_block = block.split("\n")
-    list_items = [f"<li>{line.split('.', 1)[1].strip()}</li>" for line in splited_block]
-    return f"<ol>{''.join(list_items)}</ol>"
+    list_items = [HTMLNode("li", None, line.split('.', 1)[1].strip(), False) for line in splited_block]
+    return HTMLNode("ol", None, list_items, False)
 
 def to_unordered_list(block: str) -> str:
     splited_block = block.split("\n")
-    list_items = [f"<li>{line[2:].strip()}</li>" for line in splited_block]
-    return f"<ul>{''.join(list_items)}</ul>"
+    list_items = [HTMLNode("li", None, line[2:].strip(), False) for line in splited_block]
+    return HTMLNode("ul", None, list_items, False)
--- src/test_bloctype.py
+++ src/test_bloctype.py
@@ -1,6 +1,7 @@
 import unittest
 
-from bloctype import block_to_block_type, BlockType
+from bloctype import block_to_block_type, BlockType, markdown_to_html_node
+from htmlnode import HTMLNode
 
 class TestBlockType(unittest.TestCase):
     def test_block_to_block_type_paragraph(self):
@@ -26,3 +27,36 @@ class TestBlockType(unittest.TestCase):
     def test_block_to_block_type_unordered_list(self):
         block = "- Item 1\n- Item 2\n- Item 3"
         self.assertEqual(BlockType.UNORDERED_LIST, BlockType(block_to_block_type(block)))
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+     )
+
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
