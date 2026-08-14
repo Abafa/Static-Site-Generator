d6644cdd15608a372bb0be12363ab2f86161cf0f
Abafa <flo.beg@free.fr>
2026-08-14 23:45:57 +0200

    oof!

--- src/test_bloctype.py
+++ src/test_bloctype.py
@@ -60,3 +60,5 @@ the **same** even with inline stuff
             html,
             "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
         )
+
+#OOF ! 
