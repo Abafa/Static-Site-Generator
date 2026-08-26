1e6bb36267a83baa00e6303f49889833445515fb
Abafa <flo.beg@free.fr>
2026-08-26 12:12:48 +0200

    still some things to do

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
 from textnode import TextNode, TextType
 
+def copy_content(path, destination) :
+    if os.path.isfile(path) :
+        shutil.copy(path, destination)
+    else :
+        os.mkdir(destination)
+        copy_content(path, destination)
 
-def main() -> None:
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
 
+
+
+def static_to_public () :
+    shutil.rmtree("./public", ignore_errors=True)
+    list_targets = os.listdir("./static")
+    for target in list_targets :
+        copy_content(f"./static/{target}", f"./public/{target}")
+
+
+
+def main() :
+    pass
 
 main()
