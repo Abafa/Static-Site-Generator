defe9f4c78e768663993c8fa24ba47cefe646943
Abafa <flo.beg@free.fr>
2026-09-13 13:22:47 +0200

    don't know where i'm going

--- src/generator.py
+++ src/generator.py
@@ -1,6 +1,14 @@
+from markdown_blocks import markdown_to_html_node, extract_title
+
 def generate_page(from_path, template_path, dest_path) :
     print (f"Generating page from {from_path} to {dest_path} using {template_path}")
+    file = open(from_path).read()
+    template = open(template_path).read()
+    html_noded_file = markdown_to_html_node(file)
+    title = extract_title(file)
     
+
+
 
 
     #aaaaaah Nopenopenope
