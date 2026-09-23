7b49a63aa493fd74336113720516575a7eb45b8c
Abafa <flo.beg@free.fr>
2026-09-23 11:29:49 +0200

    ???

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,9 +1,25 @@
 import os
+from pathlib import Path
 
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_pages_recursive(
+    dir_path_content: str, template_path: str, dest_dir_path: str, basepath: str
+) -> None:
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+
+
+def generate_page(
+    from_path: str, template_path: str, dest_path: str | Path, basepath: str
+) -> None:
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -19,26 +35,14 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
     to_file = open(dest_path, "w")
     to_file.write(template)
-
-
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path) :
-    for directory in os.listdir(dir_path_content):
-        new_dir_path_content = os.path.join(dir_path_content, directory)
-        new_dest_dir_path = os.path.join(dest_dir_path, directory)
-        if os.path.isfile(new_dir_path_content):
-            # Generate HTML files instead of copying the source Markdown name.
-            dest_file_name = f"{os.path.splitext(directory)[0]}.html"
-            dest_file_path = os.path.join(dest_dir_path, dest_file_name)
-            generate_page(new_dir_path_content, template_path, dest_file_path)
-        else :
-            generate_pages_recursive(new_dir_path_content, template_path, new_dest_dir_path)
-
 
 
 def extract_title(md: str) -> str:
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-import sys#
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
@@ -9,11 +9,14 @@ dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
-
-basepath = sys.argv[0]
+default_basepath = "/"
 
 
 def main() -> None:
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -21,15 +24,8 @@ def main() -> None:
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    print("Generating page...")
-    generate_pages_recursive(
-        dir_path_content,
-        template_path,
-        dir_path_public,
-        basepath
-    )
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
-
-#paenvi
